b3834fdcae2bab2526df17c4d60101e897c29227
MacDev864 <113145526+MacDev864@users.noreply.github.com>
2024-05-16 16:52:16 +0700

    Update admin.controller.ts

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import {
-  // adminAddProductService,
-  // adminAddUserService,
+  adminAddProductService,
+  adminAddUserService,
   // adminDeleteProductService,
   // adminGetProductService,
   // adminGetProductsService,
   adminGetUserService,
   adminGetUsersService,
-  // adminUpdateProductService,
-  // removeAuthService,
-  // adminUpdateAuthService,
+  adminUpdateProductService,
+  removeAuthService,
+  adminUpdateAuthService,
   // adminGetOrdersService,
   // adminGetOrderService,
   // adminDeleteSingleOrderService,
@@ -45,5 +45,21 @@ export const adminGetUsersController = (req: Request, res: TPaginationResponse) 
 export const adminGetUserController = (req: AuthenticatedRequestBody<IUser>, res: Response, next: NextFunction) =>
   adminGetUserService(req, res, next);
 
+export const adminAddUserController = (req: Request, res: Response, next: NextFunction) =>
+  adminAddUserService(req, res, next);
+
+export const adminUpdateAuthController = (req: AuthenticatedRequestBody<IUser>, res: Response, next: NextFunction) =>
+  adminUpdateAuthService(req, res, next);
+
+export const adminRemoveUserController = (req: AuthenticatedRequestBody<IUser>, res: Response, next: NextFunction) =>
+  removeAuthService(req, res, next);
+export const adminAddProductController = (req: AuthenticatedRequestBody<ProductT>, res: Response, next: NextFunction) =>
+  adminAddProductService(req, res, next);
+
+export const adminUpdateProductController = (
+  req: AuthenticatedRequestBody<ProductT>,
+  res: Response,
+  next: NextFunction
+) => adminUpdateProductService(req, res, next);
 
 export default adminGetUsersController;
