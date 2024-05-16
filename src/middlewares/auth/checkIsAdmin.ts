946b89f2356fa323047bed3ba3310958ecda8aca
MacDev864 <113145526+MacDev864@users.noreply.github.com>
2024-05-16 16:52:02 +0700

    Update checkIsAdmin.ts

--- src/middlewares/auth/checkIsAdmin.ts
+++ src/middlewares/auth/checkIsAdmin.ts
@@ -4,7 +4,30 @@ import createHttpError from 'http-errors';
 import { environmentConfig } from '../../configs/custom-environment-variables.config';
 import { IAuthRequest as IAdminRequest } from '../../interfaces';
 import { authorizationRoles } from '../../constants';
+import { customResponse } from '../../utils';
 
+export const isSuperadmin = async (req: IAdminRequest, res: Response, next: NextFunction) => {
+  const user = req?.user;
+
+  // const adminEmails = environmentConfig?.ADMIN_EMAILS && (JSON.parse(environmentConfig.ADMIN_EMAILS) as string[]);
+  const adminUser = user && user.role === authorizationRoles.superadmin ;
+
+  if (!adminUser) {
+    return res.status(403).json(
+      customResponse<object>({
+        data: {
+        },
+        success: false,
+        error: true,
+        message: "Auth Failed (Unauthorized)",
+        status: 403,
+      })
+    );
+    // return next(createHttpError(403, ``));
+  }
+
+  next();
+};
 export const isAdmin = async (req: IAdminRequest, res: Response, next: NextFunction) => {
   const user = req?.user;
 
@@ -12,10 +35,41 @@ export const isAdmin = async (req: IAdminRequest, res: Response, next: NextFunct
   const adminUser = user && user.role === authorizationRoles.admin ;
 
   if (!adminUser) {
-    return next(createHttpError(403, `Auth Failed (Unauthorized)`));
+    return res.status(403).json(
+      customResponse<object>({
+        data: {
+        },
+        success: false,
+        error: true,
+        message: "Auth Failed (Unauthorized)",
+        status: 403,
+      })
+    );
+    // return next(createHttpError(403, ``));
   }
 
   next();
 };
+export const checkRoleAdmin = async (req: IAdminRequest, res: Response, next: NextFunction) => {
+  const user = req?.user;
 
-export default { isAdmin };
+  // const adminEmails = environmentConfig?.ADMIN_EMAILS && (JSON.parse(environmentConfig.ADMIN_EMAILS) as string[]);
+  const adminUser = user && user.role === authorizationRoles.admin || user?.role === authorizationRoles.superadmin;
+
+  if (!adminUser) {
+    return res.status(403).json(
+      customResponse<object>({
+        data: {
+        },
+        success: false,
+        error: true,
+        message: "Auth Failed (Unauthorized)",
+        status: 403,
+      })
+    );
+    // return next(createHttpError(403, ``));
+  }
+
+  next();
+};
+export default { isAdmin ,isSuperadmin,checkRoleAdmin};
