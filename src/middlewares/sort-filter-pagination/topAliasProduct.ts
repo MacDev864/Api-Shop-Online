a4893dc987e81b7fa528f30e1c7cd6b6453c487e
MacDev864 <113145526+MacDev864@users.noreply.github.com>
2024-05-16 16:51:48 +0700

    Update topAliasProduct.ts

--- src/middlewares/sort-filter-pagination/topAliasProduct.ts
+++ src/middlewares/sort-filter-pagination/topAliasProduct.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 
 export const top5AliasProductsMiddleware = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    req.query.limit = '5';
+    req.query.limit = '10';
     req.query.sort = '-ratings,price';
-    req.query.limit = '5';
+    req.query.limit = '10';
     req.query.fields = '-_v';
     next();
   };
